43c7460e94c82223a38e6be269e855fa1b51a743
kiwianatra <mcshonax@gmail.com>
2025-08-17 18:55:59 +0300

    fixed

--- ql7-bank-server/_api/imports.swift
+++ ql7-bank-server/_api/imports.swift
@@ -1,11 +1,11 @@
-// import Importing
+import Importing
 
-// let API_PATH="./_api"
+let API_PATH="./_api"
 
 
-// class checkAPI {
+class checkAPI {
 
-//     case API_S = "drw.cloudstorage.drweb.link/_?api_keyvalue" 
-//     if API_s <=> || 
+    case API_S = "drw.cloudstorage.drweb.link/_?api_keyvalue" 
+    if API_s <=> || pass else print("Invalid API Key!")
     
-// }
+}
